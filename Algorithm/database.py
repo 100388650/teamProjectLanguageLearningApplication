4569f61bfeda2780ab80f5f6a1ef2fef59ce8ef0
DmitriAustin <austindmitri@gmail.com>
2020-05-04 11:50:16 +0100

    Language Learning Algorithm
    Began making functions to implement the language learning algorithm that fetches cards from the database in a phased manner. At the moment, the functions are standalone and have not been implemented into the front end. The 'Algorithm' folder will contain copies of the front end, in future versions, where I will test the algorithm on the front end.

---
+++ Algorithm/database.py
@@ -0,0 +1,69 @@
+import psycopg2
+from psycopg2 import sql
+import functions
+
+amountToFetch = 10
+newCat = "example" # The new category is defined here, when a card has been reviewed
+currentCard = "example" # The card currently being reviewed is defined here
+
+connection = psycopg2.connect(user="",
+                                  password="",
+                                  host="127.0.0.1",
+                                  port="5432",
+                                  database="postgres_db")
+cursor = connection.cursor()
+
+# Get new cards:
+# Get amountToFetch amount of new cards where category == Null
+# Get review cards:
+# If the interval between current date and the date of category change fits into
+# a fibonacci value, then get the card
+    # Get the interval from functions.py's start_of_study_check()
+# Else, ignore the card
+# Put all cards into an array
+
+def get_new_cards():
+    try:
+        # First get the new car
+        postgreSQL_select_Query = sql.SQL("select * from cards where category IS NULL").format(sql.Identifier(amountToFetch))
+        cursor.execute(postgreSQL_select_Query)
+        new_cards = cursor.fetchmany(amountToFetch) # Limits the number of rows fetched by 'amountToFetch'
+        return new_cards
+    except:
+        error = "Error"
+        return error
+        # Throw appropriate error here
+
+def return_review_cards():
+    try:
+        # First get all cards except Null ones
+        postgreSQL_select_Query = "select id, category, firstDayUsed from cards where category IS NOT NULL" # The program will parse through categories and store the ids of cards that are to be reviewed
+        cursor.execute(postgreSQL_select_Query)
+        all_cards = cursor.fetchall()
+        review_cards = []
+        i = 0
+        while i <= all_cards.length:
+            for row in all_cards:
+                cardId = row[0]
+                firstDayUsed = row[2]
+                if functions.start_of_study_check(firstDayUsed): # The program will append the card to the review cards array if the card is on a 'review day' (as defined by the fibonacci sequence)
+                    review_cards.append(cardId)
+                    i = (i+1)
+                else:
+                    i = (i+1)
+        return review_cards
+    except:
+        error = "Error"
+        return error
+        # Throw appropriate error here
+
+def update_new_card(answer, cardId, category):
+    # Updates the new cards for the current study session (category A, B and C cards)
+    newCat = functions.calculate_next_cat(answer,category) # This returns the new category
+    postgreSQL_select_Query = sql.SQL("update cards set category = ($s) where id = ($s)").format(sql.Identifier(newCat, cardId))
+    cursor.execute(postgreSQL_select_Query)
+
+def update_card(answer, cardId, category):
+    newCat = functions.calculate_next_date(answer, category) # This returns the new category
+    postgreSQL_select_Query = sql.SQL("update cards set category = ($d) where id = ($s)").format(sql.Identifier(newCat, cardId))
+    cursor.execute(postgreSQL_select_Query)
---
+++ Algorithm/functions.py
@@ -0,0 +1,67 @@
+# Functions for the spaced repetition algorithm
+from datetime import date
+from spaced_repetition import database
+import time
+
+firstDayUseProgram = date.today() # Defines the date of first use for the program
+start_minutes = time.perf_counter()
+studiedCards = [] # An array of cards already studied
+
+def gen_fib(): # A function for generating the fibonnaci sequence numbers for the daily cards
+    a,b = 1,1
+    yield a
+    yield b
+    while True:
+        a,b = b,a+b
+        yield b
+
+
+def calculate_next_date(userAnswer, cardCat):
+    # Gets the information of the card currently being studied
+    # Input card information into the exercise below
+    if userAnswer:
+        newCardCat = (cardCat + 1)
+        return newCardCat
+    else:
+        newCardCat = (cardCat - 1)
+        return newCardCat
+
+
+def calculate_next_cat(userAnswer, cardCat):
+    # Calculates the new category for new cards (Category A, B and C)
+    # And starts a timer for the card to be checked again
+    if userAnswer:
+        if cardCat == 0:
+            newCardCat = 0.1
+        elif cardCat == 0.1:
+            newCardCat = 0.2
+        else:
+            newCardCat = 1
+    else:
+        if cardCat == "A":
+            newCardCat = "A"
+        elif cardCat == "B":
+            newCardCat = "A"
+        elif cardCat == "C":
+            newCardCat = "B"
+    return newCardCat
+
+
+def start_of_study_check(firstDayUsed):
+    # Checks the intervals at the start of the study session using the days elapsed
+    currentDay = date.today()
+    daysElapsed = firstDayUsed - currentDay
+    daysElapsedProgram = currentDay - firstDayUseProgram
+    g = gen_fib()
+    fibs = [next(g) for _ in range(daysElapsed)] # Get the number of fibonacci places equal to the days the program has been used for
+    if daysElapsedProgram in fibs:
+        return True
+    else:
+        return False
+
+
+def during_study_check():
+    # Checks the intervals during the study session using the minutes elapsed
+    minute_check = time.perf_counter()
+    minutes_elapsed = start_minutes - minute_check
+    return minutes_elapsed
